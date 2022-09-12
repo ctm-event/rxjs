94bcb74e7c0960569af38597c49e921124e60e8b
Steven Nguyen <nguyenbaotrung.hcm@gmail.com>
2022-09-12 16:56:40 +0700

    Finished lecture 37

--- 2-store/src/app/about/about.component.ts
+++ 2-store/src/app/about/about.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, ViewEncapsulation} from '@angular/core';
-import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge} from 'rxjs';
+import {concat, fromEvent, interval, noop, observable, Observable, of, timer, merge, Subject, BehaviorSubject, AsyncSubject, ReplaySubject} from 'rxjs';
 import {delayWhen, filter, map, take, timeout} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
 
@@ -12,7 +12,25 @@ import {createHttpObservable} from '../common/util';
 export class AboutComponent implements OnInit {
 
     ngOnInit() {
+      const subject = new ReplaySubject();
 
+      const series$ = subject.asObservable();
+
+
+      series$.subscribe(val => console.log(`Early sub: ${val}`));
+
+      subject.next(1);
+      setTimeout(() => {
+
+        subject.next(2);
+      }, 1500)
+      subject.next(3);
+
+
+      setTimeout(() => {
+        series$.subscribe(val => console.log(`Later sub: ${val}`));
+        subject.next(4);
+      }, 3000)
 
     }
 
